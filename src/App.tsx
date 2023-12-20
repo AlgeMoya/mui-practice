cc516780fcb0ee5a4d32cd13980a79b122e872db
AlgeMoya <kjhobd2@naver.com>
2023-12-20 21:16:42 +0900

    수정함:        src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Mainpage from "./components/Mainpage";
 import { Container } from "@mui/material";
 import Login from "./components/Login";
-import SignInSide from "./pages/Signin";
-import Album from "./components/Album";
 
 function App() {
   return (
@@ -15,19 +13,11 @@ function App() {
         mx: 0,
         height: "100%",
         width: "100%",
-        // backgroundImage: "url(https://source.unsplash.com/random?wallpapers)",
-        // backgroundRepeat: "no-repeat",
-        // backgroundColor: (t) =>
-        //   t.palette.mode === "light" ? t.palette.grey[50] : t.palette.grey[900],
-        // backgroundSize: "cover",
-        // backgroundPosition: "center",
       }}
     >
       <Routes>
         <Route path="/" element={<Mainpage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signin" element={<SignInSide />} />
-        <Route path="/album" element={<Album />} />
       </Routes>
     </Container>
   );
